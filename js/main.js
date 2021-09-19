23fa6897ca303bdf60a254aad11948401c7bfdf5
KushalSandeep <79368198+KushalSandeep@users.noreply.github.com>
2021-09-19 19:11:35 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ var swiper = new Swiper(".swiper", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
-  initialSlide: 2,
+  initialSlide: 0,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
